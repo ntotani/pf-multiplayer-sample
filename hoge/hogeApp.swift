b75d70347e88e1137ba747fec7334f53ed05b528
Naoyuki Totani <nyk.totani@gmail.com>
2023-03-14 15:29:31 +0900

    Initial Commit

---
+++ hoge/hogeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hogeApp.swift
+//  hoge
+//
+//  Created by Naoyuki Totani on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct hogeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
